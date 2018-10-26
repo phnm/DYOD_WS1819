72698b5372eaf1563d81793ea00d4f1bbf04127d
Lennart L <lennart.lehmann@gmail.com>
2018-10-26 18:19:02 +0200

    use type_cast in append

--- src/lib/storage/value_segment.cpp
+++ src/lib/storage/value_segment.cpp
@@ -10,34 +10,28 @@
 #include "type_cast.hpp"
 #include "utils/assert.hpp"
 #include "utils/performance_warning.hpp"
-#include <boost/variant/get.hpp>
 
 namespace opossum {
 
 template <typename T>
 const AllTypeVariant ValueSegment<T>::operator[](const size_t offset) const {
   PerformanceWarning("operator[] used");
-  return AllTypeVariant{data.at(offset)};
+  return AllTypeVariant{_data.at(offset)};
 }
 
 template <typename T>
 void ValueSegment<T>::append(const AllTypeVariant& val) {
-  // Implementation goes here
-  if(val.type() != typeid(T)) {
-    throw std::invalid_argument("Added value does not have type of ValueSegment!");
-  }
-  data.push_back(boost::get<T>(val));
+  _data.push_back(type_cast<T>(val));
 }
 
 template <typename T>
 size_t ValueSegment<T>::size() const {
-  // Implementation goes here
-  return data.size();
+  return _data.size();
 }
 
 template <typename T>
 const std::vector<T>& ValueSegment<T>::values() const {
-  return data;
+  return _data;
 }
 
 EXPLICITLY_INSTANTIATE_DATA_TYPES(ValueSegment);
